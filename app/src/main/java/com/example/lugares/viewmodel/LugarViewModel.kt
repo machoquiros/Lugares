639c323b05c9c4469517db04b286f89cd7b5e0d2
Michael Quiros <machoquiros@gmail.com>
2022-10-14 19:34:56 -0600

    Cuarto commit, refactor de algunos archivos para poder crear una conexión con la base de datos

--- app/src/main/java/com/example/lugares/viewmodel/LugarViewModel.kt
+++ app/src/main/java/com/example/lugares/viewmodel/LugarViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.lugares.ui.home
+package com.example.lugares.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class LugarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
